ff3dffeb2f5babd36179b1fe09dae6820e5aa999
zinovyevaleks <zinovyevaleks@gmail.com>
2018-12-29 10:53:42 +0700

    несколько секций
    Как это работает?
    Где вас увидят?
    Почему мы?
    Записаться на аудит.

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/bootstrap/bootstrap.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
