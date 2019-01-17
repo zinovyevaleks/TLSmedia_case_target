cb1ec49293e1c2105eb8f803e96a0250069882f8
zinovyevaleks <zinovyevaleks@gmail.com>
2019-01-17 11:26:34 +0700

    Якорь прокрутки до формы записи на аудит Настроил формы

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,46 @@
 $(function() {
 
+    //Плавная прокрутка до якоря
+    $(".anchor_link").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+
+    //заявка на аудит
+    $("#auditForm").submit(function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8888/mail.php",
+			data: th.serialize()
+		}).done(function() {
+            dataLayer.push({'event': 'formzayavka2'});
+			alert("Спасибо, мы скоро свяжемся с Вами!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+    //Подписка на рассылку
+    $("#mailSubscription").submit(function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8888/mail.php",
+			data: th.serialize()
+		}).done(function() {
+            dataLayer.push({'event': 'formzayavka2'});
+			alert("Спасибо, мы скоро свяжемся с Вами!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
 });
